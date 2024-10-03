33e8425e8a77e0d53f2e3ded0b5c28b5d0193a75
LeoFuna <leo.funa@gmail.com>
2024-10-03 17:48:13 -0300

    generate label

---
+++ index.js
@@ -0,0 +1,85 @@
+import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
+
+export async function generateLabelPdf({ productName, productId, batchId, vlmId, quantity }) {
+    const pdfDoc = await PDFDocument.create();
+    const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);   
+    const page = pdfDoc.addPage([80, 52]);
+    const { width, height } = page.getSize();
+
+    const fontSize = 4;
+
+    const productNameObject = productName.split(' ').reduce((acc, curr) => {
+        if (acc.firstLine.length < 24) {
+            if (!acc.firstLine.length) {
+                acc.firstLine = curr;
+            } else {
+                acc.firstLine = acc.firstLine + ' ' + curr;
+            }
+        } else {
+            if (!acc.secondLine.length) {
+                acc.secondLine = curr;
+            } else {
+                acc.secondLine = acc.secondLine + ' ' + curr;
+            }
+        }
+        return acc;
+    }, { firstLine: '', secondLine: '' });
+
+    const yFistLine = productName.length > 24 ? 7 : 10
+
+    page.drawText(productNameObject.firstLine, {
+        x: 10,
+        y: height - yFistLine,
+        size: fontSize,
+        font: timesRomanFont,
+        color: rgb(0, 0, 0),
+    });
+    if (productNameObject.secondLine.length) {
+        page.drawText(productNameObject.secondLine, {
+            x: 10,
+            y: height - 13,
+            size: fontSize,
+            font: timesRomanFont,
+            color: rgb(0, 0, 0),
+        });
+    }
+    page.drawText(productId, {
+        x: 10,
+        y: height - 19,
+        size: fontSize,
+        font: timesRomanFont,
+        color: rgb(0, 0, 0),
+    });
+    page.drawLine({
+        start: { x: 7, y: height - 22 },
+        end: { x: width + 3, y: height - 22 },
+        thickness: 0.5,
+        color: rgb(0, 0, 0),
+    })
+
+    page.drawText(`LOTE: ${batchId}`, {
+        x: 10,
+        y: height - 29,
+        size: fontSize,
+        font: timesRomanFont,
+        color: rgb(0, 0, 0),
+    });
+    page.drawText(`VLM: ${vlmId}`, {
+        x: 10,
+        y: height - 36,
+        size: fontSize,
+        font: timesRomanFont,
+        color: rgb(0, 0, 0),
+    });
+    page.drawText(`QTD: ${quantity} MIL`, {
+        x: 10,
+        y: height - 43,
+        size: fontSize,
+        font: timesRomanFont,
+        color: rgb(0, 0, 0),
+    });
+    
+
+    const pdfBytes = await pdfDoc.save();
+    return Buffer.from(pdfBytes).toString('base64')
+}
---
+++ mocks/generateLabel.mock.js
@@ -0,0 +1,7 @@
+export const generateLabelMock = {
+    productName: 'TAMPA DE POTE 100ML COR AZUL ESCURO',
+    productId: '9q4n9nmcp98cn29p8n',
+    batchId: '928cn29c8nc2c92nc',
+    vlmId: 'cioucn903qnc29c8c2o9c',
+    quantity: '6',
+}
